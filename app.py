2e7ad9507b02fbb541a8e484d51f1c33db3ec624
Мейн Иван Сергеевич <Buh02@MV-SERVICE.MVSO>
2022-07-20 16:53:00 +0300

    some code

--- app.py
+++ app.py
@@ -4,17 +4,35 @@ from aiogram.types import Message
 from loader import dp, bot
 
 
+async def on_startup(_):
+    print('Бот вышел в онлайн')
+
+
 @dp.message_handler(commands=['start', 'help'])
 async def command_start(message: Message):
-    await bot.send_message(message.from_user.id, 'Приятного аппетита')
+    try:
+        await bot.send_message(message.from_user.id, 'Приятного аппетита')
+        await message.delete()
+    except:
+        await message.reply("Напишите боту в лс: \nhttps://t.me/mysimple_test_bot")
+
+@dp.message_handler(commands=['Режим работы'])
+async def open_command(message: Message):
+    await bot.send_message(message.from_user.id, 'Вс-Чт с 9:00 до 20:00')
+
+@dp.message_handler(commands=['Расположение'])
+async def open_command(message: Message):
+    await bot.send_message(message.from_user.id, 'ул. Колбасная 15')
 
 
 @dp.message_handler()
 async def echo_send(message: Message):
-    await message.answer(message.text)
-    await message.reply(message.text)
-    await bot.send_message(message.from_user.id, message.text)
+    if message.text == 'Привет':
+        await message.answer('И тебе привет!')
+    # await message.answer(message.text)
+    # await message.reply(message.text)
+    # await bot.send_message(message.from_user.id, message.text)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
