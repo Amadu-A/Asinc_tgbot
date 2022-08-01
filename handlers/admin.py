fcd67db80fd52e2131cee4b1ca1539856b1c9e0a
AmaduA <main_ivan@mail.ru>
2022-08-01 22:34:25 +0400

    added btns

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,6 +1,6 @@
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup 
-from aiogram.types import Message
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, callback_query, CallbackQuery
 from aiogram import Dispatcher
 from aiogram.dispatcher.filters import Text
 
@@ -8,16 +8,17 @@ from loader import dp, bot
 from data_base import sqlite_db
 from keyboards import admin_kb
 
-
 ID = None
+
 
 class FSMAdmin(StatesGroup):
     photo = State()
     name = State()
     description = State()
     price = State()
-    
-#Получаем ID текущего модератора
+
+
+# Получаем ID текущего модератора
 # @dp.message_handler(commands=['moderator'], is_chat_admin=True)
 async def make_changes_command(message: Message):
     global ID
@@ -26,53 +27,15 @@ async def make_changes_command(message: Message):
     await message.delete()
 
 
-#Начало диалога загрузки нового пункта меню
+# Начало диалога загрузки нового пункта меню
 # @dp.message_handler(commands='Загрузить', state=None)
 async def cm_start(message: Message):
     if message.from_user.id == ID:
         await FSMAdmin.photo.set()
         await message.reply('Загрузи фото')
 
-#Ловим первый ответ и пишем в словарь
-# @dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
-async def load_photo(message: Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:
-            data['photo'] = message.photo[0].file_id
-        await FSMAdmin.next()
-        await message.reply('Теперь введи название')
 
-#Ловим второй ответ
-# @dp.message_handler(state=FSMAdmin.name)
-async def load_name(message: Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:
-            data['name'] = message.text
-        await FSMAdmin.next()
-        await message.reply('Введи описание')
-
-#Ловим третий ответ
-# @dp.message_handler(state=FSMAdmin.description)
-async def load_description(message: Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:
-            data['description'] = message.text
-        await FSMAdmin.next()
-        await message.reply('Теперь укажи цену')
-
-#Ловим последний ответ и используем полученные данные
-# @dp.message_handler(state=FSMAdmin.price)
-async def load_price(message: Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:
-            data['price'] = float(message.text)
-
-        # async with state.proxy() as data:
-        #     await message.reply(str(data))
-        await sqlite_db.sql_add_command(state)
-        await state.finish()
-
-#Выход из состояний
+# Выход из состояний
 # @dp.message_handler(state="*", commands='Отмена')
 # @dp.message_handler(Text(equals='отмена', ignore_case=True), state="*")
 async def cansel_handler(message: Message, state: FSMContext):
@@ -84,13 +47,73 @@ async def cansel_handler(message: Message, state: FSMContext):
         await message.reply('OK')
 
 
-#Регистрируем хендлеры
+# Ловим первый ответ и пишем в словарь
+# @dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
+async def load_photo(message: Message, state: FSMContext):
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['photo'] = message.photo[0].file_id
+        await FSMAdmin.next()
+        await message.reply('Теперь введи название')
+
+
+# Ловим второй ответ
+# @dp.message_handler(state=FSMAdmin.name)
+async def load_name(message: Message, state: FSMContext):
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['name'] = message.text
+        await FSMAdmin.next()
+        await message.reply('Введи описание')
+
+
+# Ловим третий ответ
+# @dp.message_handler(state=FSMAdmin.description)
+async def load_description(message: Message, state: FSMContext):
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['description'] = message.text
+        await FSMAdmin.next()
+        await message.reply('Теперь укажи цену')
+
+
+# Ловим последний ответ и используем полученные данные
+# @dp.message_handler(state=FSMAdmin.price)
+async def load_price(message: Message, state: FSMContext):
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['price'] = float(message.text)
+
+        # async with state.proxy() as data:
+        #     await message.reply(str(data))
+        await sqlite_db.sql_add_command(state)
+        await state.finish()
+
+
+@dp.callback_query_handlers(lambda x: x.data and x.data.startswith('del '))
+async def del_callback_run(callback_query: CallbackQuery):
+    await sqlite_db.sql_delete_command(callback_query.data.replace('del ', ''))
+    await callback_query.answer(text=f'{callback_query.data.replace("del ", "")} удалена.', show_alert=True)
+
+
+@dp.message_handler(commands='Удалить')
+async def delete_item(message: Message):
+    if message.from_user.id == ID:
+        read = await sqlite_db.sql_read2()
+        for ret in read:
+            await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦена {ret[-1]}')
+            await bot.send_message(message.from_user.id, text='^^^', reply_markup=InlineKeyboardMarkup().\
+                                   add(InlineKeyboardButton(f'Удалить {ret[1]}', callback_data=f'del {ret[1]}')))
+
+
+
+# Регистрируем хендлеры
 def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(cm_start, commands=['Загрузить'], state=None)
+    dp.register_message_handler(cansel_handler, state="*", commands='Отмена')
+    dp.register_message_handler(cansel_handler, Text(equals='отмена', ignore_case=True), state="*")
     dp.register_message_handler(load_photo, content_types=['photo'], state=FSMAdmin.photo)
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_description, state=FSMAdmin.description)
     dp.register_message_handler(load_price, state=FSMAdmin.price)
-    dp.register_message_handler(cansel_handler, state="*", commands='Отмена')
-    dp.register_message_handler(cansel_handler, Text(equals='отмена', ignore_case=True), state="*")
     dp.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
